db10749ae0a37757874ed95fffa7df829679dc02
Joohyun Park <jhp527@nyu.edu>
2021-07-06 11:51:25 -0400

    test functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React, { FunctionComponent } from 'react'
+import Button from '@/components/Button'
 
-const App: FunctionComponent = ({}) => {
-  return <div>react typescript webpack !</div>
+const App: FunctionComponent = () => {
+  return (
+    <div>
+      react typescript webpack ! <Button />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react'
+import './index.scss'
+
+const Button: FunctionComponent = ({}) => {
+  return <button>button</button>
+}
+
+export default Button
