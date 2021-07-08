b6e159b000d14821b37da598bb948c43eeb06e3d
Joohyun Park <jhp527@nyu.edu>
2021-07-08 17:20:21 -0400

    add style loader to prod config

--- config/webpack.prod.ts
+++ config/webpack.prod.ts
@@ -11,19 +11,26 @@ const config: Configuration = {
   entry: 'src/index.tsx',
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: '[name].[contenthash].js',
-    publicPath: ''
+    filename: '[name].[contenthash].js'
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     }),
     new MiniCssExtractPlugin({
-      filename: 'assets/css/awa/[name].css',
+      filename: 'css/[name].css',
       chunkFilename: '[id].css'
     }),
     new CleanWebpackPlugin()
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  }
 }
 
 export default merge(commonConfig, config)
