234cd06954d72bb0c8338261555f0f63d4a0d291
jooohyunpark <jhp527@nyu.edu>
2022-01-07 09:35:43 -0500

    add test module

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { FunctionComponent } from 'react'
 import '@/styles/main.scss'
+import styles from '@/styles/test.module.scss'
 import Button from '@/components/Button'
 import Link from '@/components/Link'
 
 const App: FunctionComponent = () => {
   return (
-    <div>
+    <div className={styles.test}>
       <h2> react typescript webpack !</h2>
 
       <Button
